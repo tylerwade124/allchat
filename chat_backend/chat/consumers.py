9bc79a93bf34f52ce78afc39a16917e8797ec803
tyler <tylerwade124@gmail.com>
2023-01-03 10:32:57 -0500

    usernames displaying

--- chat_backend/chat/consumers.py
+++ chat_backend/chat/consumers.py
@@ -4,7 +4,9 @@ from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+
         self.room_group_name = 'test'
+        self.user = self.scope["user"]
 
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
@@ -22,14 +24,17 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             {
                 'type':'chat_message',
-                'message':message
+                'message':message,
+                'username': self.user.username
             }
         )
 
     def chat_message(self, event):
         message = event['message']
+        username = event['username']
 
         self.send(text_data=json.dumps({
             'type':'chat',
-            'message':message
+            'message':message,
+            'username' : username
         }))
